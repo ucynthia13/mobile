fae04af61287547c00521f42b91570e4bd14215d
ucynthia13 <cycyu614@gmail.com>
2024-05-15 22:41:18 +0200

    ...

--- components/inputwithicon.js
+++ components/inputwithicon.js
@@ -1,14 +1,18 @@
-import React, { useState } from 'react'
-import { TextInput, View } from 'react-native'
+import React from 'react';
+import { TextInput, View } from 'react-native';
 
-function InputWithIcon({ placeholder, onChangeText, secureTextEntry = false, iconName}) {
-
+function InputWithIcon({ placeholder, onChangeText, secureTextEntry = false, iconName }) {
   return (
-    <View style={{ flexDirection: 'row', alignItems: 'center', paddingHorizontal: 10, marginBottom: 10}}>
-        <MaterialCommunityIcons name={iconName} size={24} color="#ccc" style={{ marginRight: 10}} />
-        <TextInput placeholder={placeholder} onChangeText={onChangeText} secureTextEntry={secureTextEntry} value={text} style={{ flex: 1}} />
+    <View className="flex flex-row border border-gray-300 px-4 mb-4 rounded-md">
+      {/* Icon Component Here */}
+      <TextInput
+        placeholder={placeholder}
+        onChangeText={onChangeText}
+        secureTextEntry={secureTextEntry}
+        className="flex-1 py-2"
+      />
     </View>
-  )
+  );
 }
 
-export default InputWithIcon
+export default InputWithIcon;
