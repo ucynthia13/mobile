67ed7b0a6b3cb870e9a10d94e1bfe7642822a78c
ucynthia13 <cycyu614@gmail.com>
2024-05-15 22:41:02 +0200

    ...

--- screens/signupscreen.js
+++ screens/signupscreen.js
@@ -1,8 +1,7 @@
 import React, { useState } from 'react'
 import { useNavigation } from '@react-navigation/native';
 import { TouchableWithoutFeedback, View, Text, TouchableOpacity } from 'react-native';
-import MaterialIcons from '@react-native-vector-icons/material-icons'
-import InputWithIcon from '../components/inputwithicon'
+import InputWithIcon from './components/inputwithicon'
 const initialSignupData = {
     firstName: '',
     lastName: '',
@@ -25,28 +24,28 @@ function SignupScreen() {
         //Signup data handled
     }
   return (
-    <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()} >
+    <TouchableWithoutFeedback>
             
-        <View style={{flex: 1, alignItems: 'center' , marginTop: 30}}>
-            <View style={{ flexDirection: 'row', fontWeight: 'bold'}}>
-                <Text style={{ color: 'black'}}>Supa</Text>
-                <Text style={{ color: 'white'}}>Menu</Text>
+        <View className="flex flex-1 items-center mt-10 px-10 ">
+            <View className="flex-row font-bold">
+                <Text className="text-black">Supa</Text>
+                <Text className="text-black">Menu</Text>
             </View>
-                <InputWithIcon placeholder="First Name" onChangeText={(text) => handleInputChange('firstName', text)} iconName="account" />
-                <InputWithIcon placeholder="Last Name" onChangeText={(text) => handleInputChange('lastName', text)} iconName="account-search" />
-                <InputWithIcon placeholder="Email" onChangeText={(text) => handleInputChange('email', text)} iconName="email" />
-                <InputWithIcon placeholder="Password" onChangeText={(text) => handleInputChange('password', text)} secureTextEntry={true} iconName="lock" />
+                <InputWithIcon placeholder="First Name" onChangeText={(text) => handleInputChange('firstName', text)} />
+                <InputWithIcon placeholder="Last Name" onChangeText={(text) => handleInputChange('lastName', text)} />
+                <InputWithIcon placeholder="Email" onChangeText={(text) => handleInputChange('email', text)} />
+                <InputWithIcon placeholder="Password" onChangeText={(text) => handleInputChange('password', text)} secureTextEntry={true} />
 
-                <TouchableOpacity onPress={handleSubmitInput} style={{ alignItems: 'center', backgroundColor: 'orange', paddingVertical: 12}}>
-                    <Text style={{ color: 'white'}}>Register</Text>
+                <TouchableOpacity onPress={handleSubmitInput} className="items-center bg-orange-400 w-full p-4 mx-5 border-radius">
+                    <Text className="text-white font-bold">Register</Text>
                 </TouchableOpacity>
-                <View>
-                    <Text>Already have an account?</Text>
-                    <TouchableOpacity onPress={() => navigation.navigate('LoginScreen')} style={{ alignItems: 'center', backgroundColor: 'orange', paddingVertical: 16}}>
-                        <Text style={{ color: 'white'}}>Login</Text>
-                    </TouchableOpacity>
-                </View>
-            </View>
+
+                <Text className="py-5">Already have an account?</Text>
+
+                <TouchableOpacity onPress={() => navigation.navigate('LoginScreen')} className="items-center bg-orange-400 w-full p-4 mx-5">
+                    <Text className="text-white font-bold">Login</Text>
+                </TouchableOpacity>
+        </View>
     </TouchableWithoutFeedback>
   )
 }
